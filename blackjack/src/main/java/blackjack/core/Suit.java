3eb2c921fb5f96d70b132c7e3d1254f54568193d
FilipposMylonas <filiposmylonas@gmail.com>
2026-04-23 13:21:44 +0200

    Suit

---
+++ blackjack/src/main/java/blackjack/core/Suit.java
@@ -0,0 +1,8 @@
+package blackjack.core;
+
+// A Suit is one of: CLUBS, DIAMONDS, HEARTS, SPADES.
+// Interpretation: the suit of a standard playing card. Suit has no bearing
+// on score in Blackjack; it exists purely to distinguish the 52 cards.
+public enum Suit {
+    CLUBS, DIAMONDS, HEARTS, SPADES
+}
